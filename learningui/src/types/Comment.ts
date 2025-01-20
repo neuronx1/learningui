67bd4e430e7516ae78d21f0b419ac8da2e4e695b
FlorianScherlFASTAIMovies <florian.scherl@fast-ai-movies.com>
2025-01-20 18:22:10 +0100

    Updates

--- learningui/src/types/Comment.ts
+++
@@ -1,7 +0,0 @@
-export interface Comment {
-    user_prename: string;
-    user_surname: string;
-    number_thumbs_up: number;
-    text: string;
-    replies: Comment[] | null;
-}
--- learningui/src/types/RecentlyWatched.ts
+++
@@ -1,7 +0,0 @@
-export interface RecentlyWatched {
-  title: string;
-  subtitle: string;
-  videoframe: string;
-  summary: string;
-  mindmap: string;
-} 
