c354219924987528ae430921e4d24d5e1a2c4475
FlorianScherlFASTAIMovies <florian.scherl@fast-ai-movies.com>
2025-01-20 12:04:46 +0100

    SinglePageOverview

--- learningui/src/components/OverviewView.tsx
+++ learningui/src/components/OverviewView.tsx
@@ -5,23 +5,63 @@ import LinkedKnowledge from "./LinkedKnowledge";
 import Summary from "./Summary";
 import Comments from "./Comments";
 import VideoHub from "./VideoHub";
+import Title from "./Title";
 
 const MacBookPro16MainScreen: React.FC = () => {
   return (
-    <div className="w-full relative bg-white h-[1117px] overflow-hidden text-left text-base text-black font-inter">
-      <div className="flex">
-        <div className="flex-grow">
+    <div className="w-full h-full overflow-hidden text-left text-base text-black font-inter flex p-3" style={{ boxSizing: 'border-box' }}>
+      {/* Linke Spalte: 2/3 der Breite */}
+      <div className="flex flex-col w-3/5 h-full pr-3" style={{ boxSizing: 'border-box' }}>
+        {/* Obere Zeile: Video */}
+        <div className="pb-3" style={{ boxSizing: 'border-box' }}>
           <Video />
         </div>
-        <div className="flex flex-col w-[503px]">
-          <VideoHub />
-          <AIAssistant knowledgePage={false} />
+        {/* Untere Zeile: 3 Spalten */}
+        <div className="flex flex-grow h-full">
+          <div className="flex-1 h-full pr-3" style={{ boxSizing: 'border-box' }}>
+            <div className="flex flex-col h-full bg-gray-100 rounded-lg">
+              <Title title="Shared Knowledge" icon="/Knowledge.svg" />
+              <div className="flex-grow overflow-y-auto h-0">
+                <LinkedKnowledge />
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 h-full pr-3" style={{ boxSizing: 'border-box' }}>
+            <div className="flex flex-col h-full bg-gray-100 rounded-lg">
+              <Title title="Comments" icon="/Comments.svg" />
+              <div className="flex-grow overflow-y-auto h-0">
+                <Comments knowledgePage={false} />
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 h-full" style={{ boxSizing: 'border-box' }}>
+            <div className="flex flex-col h-full bg-gray-100 rounded-lg">
+              <Title title="Summary" icon="/Summary.svg" />
+              <div className="flex-grow overflow-y-auto h-0">
+                <Summary />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex">
-        <LinkedKnowledge />
-        <Comments knowledgePage={false} />
-        <Summary />
+      {/* Rechte Spalte: 1/3 der Breite */}
+      <div className="flex flex-col w-2/5 h-full">
+        {/* Obere Zeile: VideoHub */}
+        <div className="h-1/3 pb-3" style={{ boxSizing: 'border-box' }}>
+          <div className="flex flex-col h-full bg-gray-100 rounded-lg">
+            <Title title="Video Hub" icon="/VideoHub.svg" />
+            <div className="flex-grow overflow-y-auto">
+              <VideoHub /></div></div>
+        </div>
+        {/* Untere Zeile: AIAssistant */}
+        <div className="h-2/3">
+          <div className="flex flex-col h-full bg-gray-100 rounded-lg">
+            <Title title="AI Assistant" icon="/BlueBot.svg" />
+            <div className="flex-grow overflow-y-auto">
+              <AIAssistant knowledgePage={false} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
