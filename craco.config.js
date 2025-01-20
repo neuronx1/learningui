7f5f1c0b938c11828bc924a2d5782bac72008c03
FlorianScherlFASTAIMovies <florian.scherl@fast-ai-movies.com>
2025-01-20 19:14:44 +0100

    Updates

--- craco.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    webpack: {
-        configure: (webpackConfig) => {
-            webpackConfig.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx'];
-            return webpackConfig;
-        },
-    },
-};
